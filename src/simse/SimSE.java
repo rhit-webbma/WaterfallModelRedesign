e660e7dc79e4f3b29fc4b47f92fd640b51fbce1b
jurgenkr <jurgenkr@rose-hulman.edu>
2023-01-09 22:47:30 -0500

    I think got rid of all of the awt imports and errors

--- src/simse/SimSE.java
+++ src/simse/SimSE.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import simse.engine.Engine;
